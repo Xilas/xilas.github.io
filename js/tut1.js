cfddf58d9a8253e2a0322d130c901243a5e8bc50
Xilas <aaron_mallett@administrators-MacBook-Pro-25.local>
2014-12-10 14:09:25 -0600

    lots

--- js/tut1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	
-	// TOC, shows how to scroll the whole window
-	$('.toc a').click(function(){
-		$.scrollTo( this.hash, 1500, { easing:'swing' });
-		return false;
-	});
-	
-});
